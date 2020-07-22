23aa435da5e9633f91e5d285626c9b50a751ef10
Rusinova Daria <Rusinovadaria185@gmail.com>
2020-07-22 15:45:30 +0300

    ADD: logfile

--- MainForm.cpp
+++ MainForm.cpp
@@ -1,13 +1,64 @@
 #include "MainForm.h"
 #include <Windows.h>
+#include <iostream>
 using namespace EyeVisPro;
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine, int nCmdShow)
 {
     Application::EnableVisualStyles();
     Application::SetCompatibleTextRenderingDefault(false);
-
+    
     MainForm form;
     Application::Run(% form);
+
     return 0;
+}
+
+
+System::Void MainForm::MainForm_Load(System::Object^ sender, System::EventArgs^ e)
+{
+    auto start = std::chrono::system_clock::now();
+    auto start_t = std::chrono::system_clock::to_time_t(start);
+
+    std::ofstream logfile;
+    logfile.open("logfile.txt", std::ios_base::app);
+    logfile << "SESSION STARTED AT " << std::ctime(&start_t);
+    logfile.close();
+}
+
+System::Void MainForm::MainForm_Closed(System::Object^ sender, System::EventArgs^ e)
+{
+    auto end = std::chrono::system_clock::now();
+    auto end_t = std::chrono::system_clock::to_time_t(end);
+
+    std::ofstream logfile;
+    logfile.open("logfile.txt", std::ios_base::app);
+    logfile << "SESSION ENDED AT " << std::ctime(&end_t) << std::endl;
+    logfile.close();
+}
+
+void MainForm::initWidgets() {}
+void MainForm::initGraphics() 
+{
+    graphics = panel->CreateGraphics();
+    graphics->TranslateTransform(panel->Width / 2, panel->Height / 2);
+    graphics->ScaleTransform(1, -1);
+}
+
+void render()
+{
+    std::ofstream logfile;
+    logfile.open("logfile.txt", std::ios_base::app);
+    logfile << "entered : " << __TIMESTAMP__ << " : " << __FILE__ << " : " << __FUNCTION__ << " : " << __LINE__ << std::endl;
+    logfile.close();
+}
+
+System::Void MainForm::button_Click(System::Object^ sender, System::EventArgs^ e)
+{
+    std::ofstream logfile;
+    logfile.open("logfile.txt", std::ios_base::app);
+    logfile << "entered : " << __TIMESTAMP__ << " : " << __FILE__ << " : " << __FUNCTION__ << " : " << __LINE__ << std::endl;
+    logfile.close();
+
+    render();
 }
--- MainForm.h
+++ MainForm.h
@@ -1,4 +1,7 @@
 #pragma once
+#include <fstream>
+#include <chrono>
+#include <ctime>
 
 namespace EyeVisPro {
 
@@ -9,24 +12,17 @@ namespace EyeVisPro {
 	using namespace System::Data;
 	using namespace System::Drawing;
 
-	/// <summary>
-	/// —водка дл€ MainForm
-	/// </summary>
 	public ref class MainForm : public System::Windows::Forms::Form
 	{
 	public:
 		MainForm(void)
 		{
 			InitializeComponent();
-			//
-			//TODO: добавьте код конструктора
-			//
+			initWidgets();
+			initGraphics();
 		}
 
 	protected:
-		/// <summary>
-		/// ќсвободить все используемые ресурсы.
-		/// </summary>
 		~MainForm()
 		{
 			if (components)
@@ -36,24 +32,56 @@ namespace EyeVisPro {
 		}
 
 	private:
-		/// <summary>
-		/// ќб€зательна€ переменна€ конструктора.
-		/// </summary>
 		System::ComponentModel::Container ^components;
+	    System::Windows::Forms::Panel^ panel;
+		System::Windows::Forms::Button^ button;
+		Graphics^ graphics;
 
 #pragma region Windows Form Designer generated code
-		/// <summary>
-		/// “ребуемый метод дл€ поддержки конструктора Ч не измен€йте 
-		/// содержимое этого метода с помощью редактора кода.
-		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MainForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->panel = (gcnew System::Windows::Forms::Panel());
+			this->button = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// panel
+			// 
+			this->panel->BackColor = System::Drawing::SystemColors::ButtonHighlight;
+			this->panel->Location = System::Drawing::Point(13, 13);
+			this->panel->Name = L"panel";
+			this->panel->Size = System::Drawing::Size(768, 437);
+			this->panel->TabIndex = 0;
+			// 
+			// button
+			// 
+			this->button->Location = System::Drawing::Point(813, 202);
+			this->button->Name = L"button";
+			this->button->Size = System::Drawing::Size(149, 42);
+			this->button->TabIndex = 1;
+			this->button->Text = L"draw smth";
+			this->button->UseVisualStyleBackColor = true;
+			this->button->Click += gcnew System::EventHandler(this, &MainForm::button_Click);
+			// 
+			// MainForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(997, 462);
+			this->Controls->Add(this->button);
+			this->Controls->Add(this->panel);
+			this->Name = L"MainForm";
+			this->Text = L"MainForm";
+			this->Load += gcnew System::EventHandler(this, &MainForm::MainForm_Load);
+			this->Closed += gcnew System::EventHandler(this, &MainForm::MainForm_Closed);
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
+	private: void initWidgets();
+	private: void initGraphics();
+	private: System::Void button_Click(System::Object^ sender, System::EventArgs^ e);
+
+	private: System::Void MainForm_Load(System::Object^ sender, System::EventArgs^ e);
+	private: System::Void MainForm_Closed(System::Object^ sender, System::EventArgs^ e);
 	};
 }
